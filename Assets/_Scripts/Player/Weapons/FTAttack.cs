10aa131145f8929364da59e12d1f100f2ef74b01
Dominic S <dsal42@unm.edu>
2025-04-21 00:18:22 -0600

    New playtest build+ starting fuel adjustment

--- Assets/_Scripts/Player/Weapons/FTAttack.cs
+++ Assets/_Scripts/Player/Weapons/FTAttack.cs
@@ -25,7 +25,8 @@ public class FTAttack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        healthBar.UpdateHealthBar(fuel, maxFuel);
+        healthBar2.UpdateHealthBar(fuel, maxFuel);
     }
 
     // Update is called once per frame
