736230851d5aa6c0fec563f9211f723a7a8bf9b5
Sho Komiyama <51871965+KMYMsho@users.noreply.github.com>
2025-05-09 10:46:46 -0600

    added quit button  in main menu

--- Assets/_Scripts/UI/MainMenuUI.cs
+++ Assets/_Scripts/UI/MainMenuUI.cs
@@ -6,17 +6,25 @@ public class MainMenuUI : MonoBehaviour
 {
 
     [SerializeField] private Button startButton;
+    [SerializeField] private Button quitButton;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         startButton.onClick.AddListener(StartGame);
+        quitButton.onClick.AddListener(QuitGame);
     }
 
     void StartGame()
     {
         Debug.Log("Loading GameLevel");
         SceneManager.LoadSceneAsync("TutorialScene");
+    }
+
+    void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
     }
 }
